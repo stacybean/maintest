6c55f48913da26ee524f6efb52c731318113b612
Stacy Bean <stacy99@hotmail.com>
2018-06-02 13:25:57 +1200

    Commit from within IDE

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # maintest
 This is an update
+This is another updater for versin control
--- file1.py
+++ file1.py
@@ -1,5 +1,6 @@
 #This is a change
 def greet(msg):
     print(msg)
-//adding a second comment
+#//adding a second comment
+#//adding comment for git push within IDE
 greet("Hello everybody")
